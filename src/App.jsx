1f50a258457a3e03f7247bf6b3ad82519af85ea6
Leandro Plaza <leandro.plaza@fce.uncu.edu.ar>
2024-03-13 13:07:05 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import CartWidget from './components/CartWidget';
 import ItemListContainer from './components/ItemListContainer';
+import './App.css'; // Importa los estilos CSS para la aplicación
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container"> {/* Agrega una clase para el contenedor de la aplicación */}
       <NavBar />
       <CartWidget />
       <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
     </div>
   );
-  
 }
 
 export default App;
